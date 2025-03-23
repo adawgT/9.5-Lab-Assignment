5b79eb05208008063655cfb2438c154531e7010b
adawgT <nguy1434@msu.edu>
2025-03-23 20:28:50 +0000

    edit

--- my-react-app/src/main.js
+++ my-react-app/src/main.js
@@ -1,8 +1,8 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from './vite.svg'
-import { setupCounter } from './counter.js'
 import myImage from './myImage.png';
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
+
 
 document.querySelector('#app').innerHTML = `
   <div>
